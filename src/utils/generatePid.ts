dcf70af159bd7816174d90f4a37b56f4f13f162c
Shashank Acharya <acharya.s@icloud.com>
2023-04-22 14:10:11 +0800

    Fix pid generation bug

--- src/utils/generatePid.ts
+++ src/utils/generatePid.ts
@@ -7,7 +7,7 @@ export function generatePid(): string {
 	const charactersLength = characters.length;
 	const bytes = randomBytes(LENGTH);
 	let result = new Array(LENGTH);
-	for (let i = 0; i < length; i++) {
+	for (let i = 0; i < LENGTH; i++) {
 		result[i] = characters[bytes[i] % charactersLength];
 	}
 	return result.join('');
