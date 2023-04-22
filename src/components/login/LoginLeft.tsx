33a8edc9b07e9e6cea4c2a83f6a8cb3f4341200f
Shashank Acharya <acharya.s@icloud.com>
2023-04-22 20:56:43 +0800

    Fix UI bugs

--- src/components/login/LoginLeft.tsx
+++ src/components/login/LoginLeft.tsx
@@ -3,9 +3,11 @@ import Logo from "../common/logo/Logo";
 
 export default function LoginLeft() {
 	return (
-		<div className="p-16 w-full h-full hidden lg:flex flex-col gap-2 justify-center with-bg-1">
-			<Logo size={'4em'} className="w-full"/>
-			<div className="text-2xl opacity-50 max-w-md">{pageText.login.tagline}</div>
+		<div className="p-16 w-full h-full hidden lg:flex flex-col gap-2 justify-center items-center with-bg-1">
+			<div className="flex flex-col gap-2 max-w-md justify-center items-center">
+				<Logo size={'4em'} className="w-full"/>
+				<div className="text-2xl opacity-50">{pageText.login.tagline}</div>
+			</div>
 		</div>
 	)
 }
