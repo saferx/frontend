3196dbd098c11a482e212fabdeea6701201c2dd7
Shashank Acharya <acharya.s@icloud.com>
2023-04-22 16:06:14 +0800

    Update contractDetails.ts

--- src/constants/contractDetails.ts
+++ src/constants/contractDetails.ts
@@ -1,6 +1,6 @@
 export const contractDetails = ({
-	userRegistryAddress: '0xec92A1016BcC3BDfd5e2880e2497b372Eb8C16d5',
-	medicationAddress: '0xC2B5f9E1fE8Fd1706fe58BDE87c75399E22A409c',
+	userRegistryAddress: '0x306056b5596DC197599b1e992FB889242999af0F',
+	medicationAddress: '0x03265ab47aBb4A5Ede699f42C905fc18C766c624',
 	network: 'mumbai',
 	indexer: 'https://mumbai-indexer.sequence.app'
 })
