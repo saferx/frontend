5b211866f9dcf8078a6726fb742800eb1ce6d5b4
Shashank Acharya <acharya.s@icloud.com>
2023-04-22 19:52:29 +0800

    Add HistoryItemOverlay

---
+++ src/components/common/history-table/HistoryItemOverlay.tsx
@@ -0,0 +1,58 @@
+import { relativeToNow } from "@/utils/parseDate";
+import { IconX } from "@tabler/icons-react";
+
+interface HistoryItem {
+	name?: string;
+    dosage?: number;
+    timestamp: number;
+    remarks?: string;
+    pid?: string;
+    tokenId: number;
+    patient: string;
+    pharmacist: string;
+    quantity: number;
+}
+
+export default function HistoryItemOverlay(props: ({ historyItem: HistoryItem, close: () => void})) {
+	console.log(props.historyItem)
+	return (
+		<div className="fixed top-0 left-0 p-4 w-screen h-screen bg-black/80 dark:bg-slate-700/50 z-50 flex items-center justify-center">
+			<div className="w-full max-w-md with-bg-1 px-8 py-12 rounded-lg flex flex-col gap-4">
+				<div className="flex justify-between items-center">
+					<div className="text-lg font-medium">Redemption Details</div>
+					<button onClick={props.close}><IconX/></button>
+				</div>
+				<div className="flex justify-between items-center">
+					<div className="flex flex-col gap-1">
+						<div className="text-sm">Prescription ID</div>
+						<div className="opacity-50">{props.historyItem.pid}</div>
+					</div>
+					<div className="flex flex-col gap-1">
+						<div className="text-sm">Time</div>
+						<div className="opacity-50">{relativeToNow(props.historyItem.timestamp)}</div>
+					</div>
+				</div>
+				<div className="flex flex-col gap-1 mt-4">
+					<div className="text-sm">Name</div>
+					<div className="opacity-50">{props.historyItem.name}</div>
+				</div>
+				<div className="flex flex-col gap-1">
+					<div className="text-sm">Quantity</div>
+					<div className="opacity-50">{props.historyItem.quantity}</div>
+				</div>
+				<div className="flex flex-col gap-1">
+					<div className="text-sm">Remarks</div>
+					<div className="opacity-50">{props.historyItem.remarks}</div>
+				</div>
+				<div className="flex flex-col gap-1 mt-4">
+					<div className="text-sm">Patient</div>
+					<div className="opacity-50">{props.historyItem.patient.slice(0,20)}...</div>
+				</div>
+				<div className="flex flex-col gap-1">
+					<div className="text-sm">Pharmacist</div>
+					<div className="opacity-50">{props.historyItem.pharmacist.slice(0, 20)}...</div>
+				</div>
+			</div>
+		</div>
+	)
+}
--- src/components/common/history-table/HistoryTable.tsx
+++ src/components/common/history-table/HistoryTable.tsx
@@ -1,20 +1,36 @@
 
 import useUserRegistry from "@/hooks/useUserRegistry";
 import { relativeToNow } from "@/utils/parseDate";
-import React from "react";
+import React, { useState } from "react";
+import HistoryItemOverlay from "./HistoryItemOverlay";
 
 interface HistoryComponentProps {
 	history?: {
-		quantity: number;
+		name?: string;
+		dosage?: number;
+		timestamp: number;
+		remarks?: string;
+		pid?: string;
+		tokenId: number;
 		patient: string;
 		pharmacist: string;
-		tokenId: number;
-		timestamp: number;
+		quantity: number;
 	}[] | null
 }
 
 export default function HistoryTable(props: HistoryComponentProps) {
 	const { role } = useUserRegistry()
+	const [selectedHistory, setSelectedHistory] = useState<({
+		name?: string;
+		dosage?: number;
+		timestamp: number;
+		remarks?: string;
+		pid?: string;
+		tokenId: number;
+		patient: string;
+		pharmacist: string;
+		quantity: number;
+	}) | null>(null)
 	if (props.history === undefined) return <></>
 	if (props.history == null) return (
 		<div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative max-w-lg" role="alert">
@@ -27,25 +43,28 @@ export default function HistoryTable(props: HistoryComponentProps) {
 		</div>
 	)
 	return (
+		<>
 		<table className="table-auto overflow-scroll w-full">
 			<thead className="text-sm text-left uppercase opacity-40">
 				<th className="py-3">Time</th>
-				<th className="py-3">Token ID</th>
-				{ role !== 'Patient' && <th className="py-3">From Patient</th>}
-				{ role !== 'Pharmacist' && <th className="py-3">To Pharmacist</th>}
+				<th className="py-3">Medicine</th>
 				<th className="py-3">Quantity</th>
+				{ role === 'Pharmacist' && <th className="py-3">From Patient</th>}
+				{ role !== 'Pharmacist' && <th className="py-3">To Pharmacist</th>}
 			</thead>
 			<tbody>
 				{props.history.sort(h => h.timestamp).reverse().map((historyItem, idx) => (
-				<tr key={idx}>
-					<td className="py-2">{relativeToNow(historyItem.timestamp)}</td>
-					<td className="py-2">{historyItem.tokenId}</td>
-					{role !== 'Patient' && <td className="py-2">{historyItem.patient.slice(0,10)}...</td>}
-					{role !== 'Pharmacist' && <td className="py-2">{historyItem.pharmacist.slice(0,10)}...</td>}
-					<td className="py-2">{historyItem.quantity}</td>
+				<tr key={idx} className="with-bg-1 with-hover" onClick={() => setSelectedHistory(historyItem)}>
+					<td className="p-2">{relativeToNow(historyItem.timestamp)}</td>
+					<td className="p-2">{historyItem.name}</td>
+					<td className="p-2">{historyItem.quantity}</td>
+					{role === 'Pharmacist' && <td className="p-2">{historyItem.patient.slice(0,10)}...</td>}
+					{role !== 'Pharmacist' && <td className="p-2">{historyItem.pharmacist.slice(0,10)}...</td>}
 				</tr>
 				))}
 			</tbody>
 		</table>
+		{selectedHistory && <HistoryItemOverlay close={() => setSelectedHistory(null)} historyItem={selectedHistory}/>}
+		</>
 	);
 }
